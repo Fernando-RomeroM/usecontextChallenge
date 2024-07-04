78e0d38579c4b0a6fde1b75dd9298c9b283b98a6
Fernando Romero Marchante <159028132+Fernando-RomeroM@users.noreply.github.com>
2024-07-04 15:03:02 +0200

    Entrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import './App.css'; // Archivo de estilos CSS
+import React, { useContext } from 'react';
+import { ThemeProvider, ThemeContext } from './themes/ThemeContext';
+import RoutesApp from './routes/RoutesApp';
+import './App.css';
 
 const App = () => {
+  const { theme } = useContext(ThemeContext);
+
   return (
-<></>
+    <ThemeProvider>
+      <div className={`App ${theme}`}>
+        <RoutesApp />
+      </div>
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from '../themes/ThemeContext';
+
+const Button = () => {
+  const { toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <button onClick={toggleTheme}>Toggle Theme</button>
+  );
+};
+
+export default Button;
